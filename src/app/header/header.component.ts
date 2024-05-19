d21b208a2f472194c4df08c6cb5419c4a59e4e4d
Stephen McKenna <mckennask00@gmail.com>
2024-05-18 20:34:23 -0400

    Development (#4)
    * add animated name header
    
    * change some colors, make it look a lil different
    
    * teeny tiny change
    
    * add memoji with colorful blob and scroll indicator
    
    * make scroll indicator its own component, increase component budget
    
    * increase budgets

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {ScrollIndicatorComponent} from "./scroll-indicator/scroll-indicator.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ScrollIndicatorComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
