c9005adb1f4ecba384feca5669e4097b46228572
Stephen McKenna <mckennask00@gmail.com>
2024-05-18 20:28:32 -0400

    make scroll indicator its own component, increase component budget

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {ScrollIndicatorComponent} from "./scroll-indicator/scroll-indicator.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    ScrollIndicatorComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
